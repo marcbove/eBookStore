4dd96333d7be8809bc9868b76e5f0d2d44e63db3
Marc Bove <marc.boveg@estudiants.urv.cat>
2019-02-23 16:56:29 +0100

    Test Commit:

--- eBookStore/src/java/HelloWorldResource.java
+++ eBookStore/src/java/HelloWorldResource.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 1997-2010 Oracle and/or its affiliates. All rights reserved.
  *
  * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
@@ -23,13 +23,13 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
  * Contributor(s):
- *
+ * 
  * The Original Software is NetBeans. The Initial Developer of the Original
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -41,11 +41,48 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-package helloworld;
 
-import javax.ws.rs.core.Application;
 
-@javax.ws.rs.ApplicationPath("resources")
-public class ApplicationConfig extends Application {
 
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+
+/**
+ * REST Web Service
+ *
+ * @author mkuchtiak
+ */
+
+@Stateless
+@Path("/greeting")
+public class HelloWorldResource {
+
+    @EJB
+    private NameStorageBean nameStorage;
+    /**
+     * Retrieves representation of an instance of helloworld.HelloWorldResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("text/html")
+    public String getGreeting() {
+        return "<html><body><h1>Hello "+nameStorage.getName()+"!</h1></body></html>";
+    }
+
+    /**
+     * PUT method for updating an instance of HelloWorldResource
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("text/plain")
+    public void setName(String content) {
+        nameStorage.setName(content);
+    }
 }
--- eBookStore/src/java/NameStorageBean.java
+++ eBookStore/src/java/NameStorageBean.java
@@ -40,7 +40,7 @@
  * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
-package helloworld;
+
 
 import javax.ejb.Singleton;
 
