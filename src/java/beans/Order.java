e52346571dd259df8b60242d0ecf18a2987ab48c
Marc Bove <marc.boveg@estudiants.urv.cat>
2019-05-28 19:08:31 +0200

    First commit

---
+++ src/java/beans/Order.java
@@ -0,0 +1,66 @@
+package beans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@Entity
+@XmlRootElement
+public class Order implements Serializable 
+{
+    @Column(name = "ORDER_ID")
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    private int orderId;
+    
+    @JoinColumn(name = "BOOKS") @ManyToMany(cascade=CascadeType.PERSIST)
+    private ArrayList<Book> books;
+    @JoinColumn(name = "CUSTOMER") @ManyToOne(cascade=CascadeType.PERSIST)
+    private Customer customer;
+    
+    public Order()
+    {
+        
+    }
+    
+    public Order(int orderId)
+    {
+        this.orderId = orderId;
+    }
+    
+    public int getId()
+    {
+        return orderId;
+    }
+
+    @XmlTransient
+    public ArrayList<Book> getBooks() 
+    {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) 
+    {
+        this.books = books;
+    }
+
+    public Customer getCustomer() 
+    {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) 
+    {
+        this.customer = customer;
+    }
+}
+
